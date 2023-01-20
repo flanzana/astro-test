02156fd6162e3684706a0878fb840b0b1008a3bd
zana.flander <zana.flander@kiwi.com>
2023-01-20 18:31:31 +0100

    feat: rewrite CSS to TailwindCSS

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,18 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: colors.violet,
+        secondary: colors.pink,
+      },
+      gridTemplateColumns: {
+        cards: 'repeat(auto-fit, minmax(24ch, 1fr))',
+      },
+    },
   },
   plugins: [],
 }
